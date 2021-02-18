abadca49055e3fc0c7f9989edf7c57b09752f3a9
QuangNguyen <quangnguyenos0302@gmail.com>
2021-02-18 10:24:31 +0700

    upload

---
+++ client/CourseOnline/src/main/java/com/fsoft/client/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.fsoft.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping("")
+	public ModelAndView home(Model model) {
+		ModelAndView mav = new ModelAndView("web/index");
+		model.addAttribute("message", "hello");
+		return mav;
+	}
+
+}
